bf8bde7cb413fe03b26df5b8a5c1cbc9e3383ada
lichingchester <liching.chester@gmail.com>
2016-12-03 03:41:53 +0800

    ongoing insert new record into json file

--- assignment/js/register.js
+++ assignment/js/register.js
@@ -2,48 +2,44 @@
  * Created by chingli on 2/12/2016.
  */
 
-// function readTextFile(file, callback) {
-//     var rawFile = new XMLHttpRequest();
-//     rawFile.overrideMimeType("application/json");
-//     rawFile.open("GET", file, true);
-//     rawFile.onreadystatechange = function(){
-//         if(rawFile.readyState === 4 && rawFile.status == "200"){
-//             callback(rawFile.responseText);
-//         }
-//     }
-//     rawFile.send(null);
-// }
-//
-// $(document).ready(function () {
-//
-//     readTextFile("../lib/data.json", function (text){
-//         var data = JSON.parse(text);
-//         alert(data);
-//         alert(text);
-//     });
-//
-// })
-//
+var username = getParameterByName("uname", document.url);
+var password = getParameterByName("pwd", document.url);
+var address = getParameterByName("address", document.url);
+var tel = getParameterByName("tel", document.url);
 
     $(document).ready(function(){
 
-        AJAX_JSON_Req( '../lib/data.json' );
+        $.ajax({
+            url: "json/user.json",
+            dataType: "text",
+            success: function(data){
+                var users = JSON.parse(data);
+                users.users.push(
+                    {
+                        "username": username,
+                        "password" : password,
+                        "address": address,
+                        "tel" : tel,
+                        "point": 0
+                    }
+                );
+                alert(users.users[0].username+"\nsuccess!");
+            },
+            error: function(data){
+                alert("ASdf"+data);
+            }
+        })
+
     })
 
-
-function AJAX_JSON_Req( url )
-{
-    var AJAX_req = new XMLHttpRequest();
-    AJAX_req.open( "GET", url, true );
-    AJAX_req.setRequestHeader("Content-type", "application/json");
-
-    AJAX_req.onreadystatechange = function()
-    {
-        if( AJAX_req.readyState == 4 && AJAX_req.status == 200 )
-        {
-            var response = JSON.parse( AJAX_req.responseText );
-            alert(response);
-        }
+function getParameterByName(name, url) {
+    if (!url) {
+        url = window.location.href;
     }
-    AJAX_req.send();
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
