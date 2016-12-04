d78e1eb033c48c59325d7311676934be34714402
lichingchester <liching.chester@gmail.com>
2016-12-04 21:36:33 +0800

    img converted

---
+++ assignment/js/product_type.js
@@ -0,0 +1,25 @@
+/**
+ * Created by lichi on 4/12/2016.
+ */
+
+var type = getParameterByName("type", document.url);
+
+$(document).ready(function(){
+
+});
+
+
+
+
+// function
+function getParameterByName(name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
